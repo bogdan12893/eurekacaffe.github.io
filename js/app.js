36003003052efa2c1d2cd3e86194e1d8017eabaf
Bogdan Cadar <bogdan1281993@gmail.com>
2018-08-20 16:59:19 +0300

    fixed some issues with local storage

--- js/app.js
+++ js/app.js
@@ -1,5 +1,92 @@
 $(document).ready(function(){
-   /*const foods = [
+    //price range for food
+    $('#flat-slider').slider({
+        max: 30,
+        range: true,
+        values: [5, 20],
+        change: function(e, ui){
+            foodPriceRange(ui.values[0], ui.values[1]);
+        }
+    });
+
+    let current = $('#flat-slider').slider("option", "values");
+
+    foodPriceRange(current[0], current[1]);
+
+    function foodPriceRange(min, max){
+        $('#rangeMin').text(`$${min}`);
+        $('#rangeMax').text(`$${max}`);
+        //if condition pass the card will display in foodResult
+        let foodResult ='';
+        foods.map(food => {
+            if(food.price >= min && food.price <= max){
+                //creating the card
+                foodResult += `
+                <div class="col-sm-3">
+                   <div class="card">
+                       <img class="card-img-top" src="${food.photo}" alt="broken photo">
+                       <div class="card-body">
+                           <h5 class="card-title text-center">${food.name}</h5>
+                           <h6 class="text-center">${food.category}</h6>
+                           <p class="card-text text-center">$${food.price}</p>
+                       </div>
+                   </div>
+               </div>
+               `;
+            }
+        }).join('');
+        
+        $("#food-card").html(foodResult);
+    }
+    //Table of food in food.html
+    function listFood(){
+        let listFood = '';
+        let row = 1;
+        foods.map(food =>{
+                //creating the row
+                listFood += `                  
+                        <tr>
+                        <th scope="row">${row++}</th>
+                        <td>${food.index}</td>
+                        <td>${food.name}</td>
+                        <td>${food.category}</td>
+                        <td>$${food.price}</td>
+                        <td>${food.photo}</td>
+                        <td><button id="delete-btn"><i class="fas fa-trash-alt"></i></button></td>
+                        </tr>
+               `;
+        }).join('');
+        foodPriceRange(current[0], current[1]);
+        $(".food-list-view").html(listFood);
+    }
+    listFood();
+    
+    $("#add-food-form").submit(e => {
+        e.preventDefault();
+
+        let index = $("#input-index").val();
+        let name = $("#input-name").val();
+        let category = $("#input-category").val();
+        let price = $("#input-price").val();
+        let photo = $("#input-photo").val();
+        addNewFood(index, name, category, price, photo);
+        listFood();
+
+    });
+    //not fully functional
+    $("#delete-btn").bind("click", function(){ 
+        deleteFood();
+        listFood();
+    });
+
+    $("#mock-btn").bind("click", function(){ 
+        foods = MOCKED_FOOD;
+        listFood();
+    });
+});
+
+    //Add foods
+    const MOCKED_FOOD = [
         {
             id: 0,
             name: "Chicken Manchurian",
@@ -70,121 +157,45 @@ $(document).ready(function(){
             price: 8.99,
             photo: "https://d2e147lbltb8cj.cloudfront.net/r/i/2cf3915d-9e8f-44d1-9335-eed2cf943285_10x_299_195_75"
         }
-   ];*/
-    //Add foods
-    //empty array where the foods will be stored
+   ];
+   //empty array where the foods will be stored
     let foods = [];
-
     //food object
-    function food(name, category, price, photo){
+    function food(id, name, category, price, photo){
+        this.id = id;
         this.name = name;
         this.category = category;
         this.price = price;
         this.photo = photo;
     }
     //adding a new food
-    function addNewFood(name, category, price, photo){
-        let foodItem = new food(name, category, price, photo);
+    function addNewFood(id, name, category, price, photo){
+        let foodItem = new food(id, name, category, price, photo);
         foods.push(foodItem);
         saveFoods();
     }
-    //Remove food
-    function removeFood(index){
-        foods.splice(index, 1);
-    }
-
-    //addNewFood('qwe','qwe','12','https://d2e147lbltb8cj.cloudfront.net/r/i/77344844-b13a-453d-a64d-409b139e942a_10x_299_195_75');
-    
-    /*saving the data to localstorage
-    let foodsArr = JSON.stringify(foods);
-    let foodsSet = localStorage.setItem("savedFoods", foodsArr);
-    foodsArr = localStorage.getItem("savedFoods");
-    let foodsObj = JSON.parse(foodsArr);*/
-
     //save data to local storage
     function saveFoods(){
         let str = JSON.stringify(foods);
-        localStorage.setItem("savedFoods", str);
+        localStorage.setItem("foods", str);
     }
     //get data to local storage
     function getFoods(){
-        let str = localStorage.getItem("savedFoods");
-        savedFoods = JSON.parse(str);
-        debugger;
+        let str = localStorage.getItem("foods");
+        foods = JSON.parse(str);
+        if(!foods){
+            foods = [];
+        }
     }
     getFoods();
-
-    //price range for food
-    $('#flat-slider').slider({
-        max: 30,
-        range: true,
-        values: [5, 20],
-        change: function(e, ui){
-            foodPriceRange(ui.values[0], ui.values[1]);
-        }
-    });
-
-    let current = $('#flat-slider').slider("option", "values");
-
-    foodPriceRange(current[0], current[1]);
-
-    function foodPriceRange(min, max){
-        $('#rangeMin').text(`$${min}`);
-        $('#rangeMax').text(`$${max}`);
-        //if condition pass the card will display in foodResult
-        let foodResult ='';
-        foods.map(food => {
-            if(food.price >= min && food.price <= max){
-                //creating the card
-                foodResult += `
-                <div class="col-sm-3">
-                   <div class="card">
-                       <img class="card-img-top" src="${food.photo}" alt="broken photo">
-                       <div class="card-body">
-                           <h5 class="card-title text-center">${food.name}</h5>
-                           <h6 class="text-center">${food.category}</h6>
-                           <p class="card-text text-center">$${food.price}</p>
-                       </div>
-                   </div>
-               </div>
-               `;
+    //delete data to local storage
+    //not fully functional
+    function deleteFood(index){ 
+        for(let i = 0; i < foods.length; i++){
+            if(foods[i].id){
+                foods.splice(index, 1);
             }
-        }).join('');
-        
-        $("#food-card").html(foodResult);
+        }
+        saveFoods();
     }
-
-    //Table of food in food.html
-    function listFood(){
-        let listFood = '';
-        let row = 1;
-        foods.map(food =>{
-                //creating the row
-                listFood += `                  
-                        <tr>
-                        <th scope="row">${row++}</th>
-                        <td>${food.name}</td>
-                        <td>${food.category}</td>
-                        <td>$${food.price}</td>
-                        <td>${food.photo}</td>
-                        <td><button id="delete-btn"><i class="fas fa-trash-alt"></i></button></td>
-                        </tr>
-               `;
-        }).join('');
-        foodPriceRange(current[0], current[1]);
-        $(".food-list-view").html(listFood);
-    }
-    
-    $("#add-food-form").submit(e => {
-        e.preventDefault();
-
-        let name = $("#input-name").val();
-        let category = $("#input-category").val();
-        let price = $("#input-price").val();
-        let photo = $("#input-photo").val();
-        addNewFood(name, category, price, photo);
-        listFood();
-
-    });
-});
 
